dea4f67541552e5e53fbb4f39ccaef8c6091ba19
Khumairotul <100303164+Khumairotul@users.noreply.github.com>
2022-02-28 12:12:17 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,11 +5,36 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script*/
+/**
+* Membuat variabel loginForElement untuk tampilan form
+* @constan {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+* Membuat variabel inputEmailElement untuk tampilan input email
+* @constan {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+* Membuat variabel inputPasswordElement untuk tampilan input password
+* @constan {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+* Membuat variabel expectedEmail untuk menyimpan informasi email sementara
+* @constan {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+* Membuat variabel expectedPassword untuk menyimpan informasi password sementara
+* @constan {string}
+*/
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
